ac0a3628256fff9a9668227797adfbddca5f992a
bittu <bittu@boy>
2026-05-20 06:27:44 +0530

    add cicd job v1.1 -- add algo snapshot

--- tests/TC-RM-PKG-006 — Lookup helper accuracy for snapshot item selection.py
+++ tests/TC-RM-PKG-006 — Lookup helper accuracy for snapshot item selection.py
@@ -1,7 +1,8 @@
+from config import BASE_URL, BASE_UNAME, PASSWORD, PKG_CONFIG
+
 import pytest
 import re
 from playwright.sync_api import Page, expect
-from config import BASE_URL, BASE_UNAME, PASSWORD, PKG_CONFIG
 
 
 @pytest.fixture(scope="session")
@@ -15,26 +16,26 @@ def test_example(page: Page) -> None:
     page.get_by_role("textbox", name="Username").fill(BASE_UNAME)
     page.get_by_role("textbox", name="Password").click()
     page.get_by_role("textbox", name="Password").fill(PASSWORD)
-    page.get_by_role("textbox", name="Password").press("Enter")
     page.get_by_role("button", name="Login").click()
-    page.get_by_role("cell", name=PKG_CONFIG).click()
-    page.get_by_role("button", name="Take Snapshot").click()
-    page.get_by_role("button", name="Cancel").click()
+    page.get_by_role("link", name=PKG_CONFIG, exact=True).click()
     page.get_by_role("button", name="Data Model C2M2.9_129").click()
     page.get_by_role("menuitem", name="C2M_Demo_2.9x", exact=True).click()
     page.get_by_role("button", name="Take Snapshot").click()
     page.get_by_role("combobox").filter(has_text="DEMO-env- pipeline").click()
-    page.get_by_role("option", name="C2M-DEMO-29X-").click()
-    page.locator(".lucide.lucide-search").click()
+    page.get_by_text("C2M-DEMO-29X-").click()
     page.get_by_role("textbox", name="Search dataset...").click()
-    page.get_by_role("textbox", name="Search dataset...").fill("acc")
-    page.get_by_text("ess Group").click()
+    page.get_by_role("textbox", name="Search dataset...").fill("algo")
+    page.get_by_role("option", name="Algorithm", exact=True).click()
     page.get_by_role("button", name="Continue").click()
     page.get_by_label(
         "Key-Based").get_by_role("button").filter(has_text=re.compile(r"^$")).click()
-    page.get_by_role("row", name="Select row MICHELIN Michelin").get_by_label(
-        "Select row").click()
+    page.get_by_role("columnheader", name="Name").get_by_placeholder(
+        "Search...").click()
+    page.get_by_role("columnheader", name="Name").get_by_placeholder(
+        "Search...").fill("INFO")
+    page.get_by_role(
+        "row", name="Select row C1-ACCT-INFO").get_by_label("Select row").click()
     page.get_by_role("button", name="Continue").click()
     page.get_by_role("button", name="Continue").click()
     page.get_by_role("button", name="Take Snapshot").click()
-    page.get_by_role("cell", name="MICHELIN").click()
+    page.get_by_text("Snapshot creation started").click()
