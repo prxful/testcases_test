527a15da6fbd8750c2f51796e53949ed0815fbab
bittu <bittu@boy>
2026-05-20 06:36:50 +0530

    add cicd job v1.1 -- modify PKG-003

--- tests/TC-RM-PKG-003 – Mandatory Field Validation on package creation (negative).py
+++ tests/TC-RM-PKG-003 – Mandatory Field Validation on package creation (negative).py
@@ -1,7 +1,7 @@
+from config import BASE_URL, BASE_UNAME, PASSWORD
 import pytest
 import re
 from playwright.sync_api import Page, expect
-from config import BASE_URL, BASE_UNAME, PASSWORD
 
 
 @pytest.fixture(scope="session")
@@ -15,27 +15,8 @@ def test_example(page: Page) -> None:
     page.get_by_role("textbox", name="Username").fill(BASE_UNAME)
     page.get_by_role("textbox", name="Password").click()
     page.get_by_role("textbox", name="Password").fill(PASSWORD)
-    page.get_by_role("textbox", name="Password").press("Enter")
     page.get_by_role("button", name="Login").click()
     page.get_by_role("button", name="New Package").click()
     page.get_by_role("button", name="Save").click()
     page.get_by_text("Package Name is required").click()
-    page.locator("#package-name").click()
-    page.locator("#package-name").fill("AUTO_TEST_V3")
-    page.get_by_role("textbox", name="Enter package description...").click()
-    page.get_by_role("textbox", name="Enter package description...").fill(
-        "Test Validation by Automation")
-    page.locator("#package-type").click()
-    page.get_by_text("Configuration", exact=True).click()
-    page.locator("#package-name").click()
-    page.locator("#package-name").dblclick()
-    page.locator("#package-name").fill("\\")
-    page.get_by_text("Package Name is required").click()
-    page.get_by_role("button", name="Save").click()
-    page.get_by_text("Package Name is required").click()
-    page.locator("#package-name").click()
-    page.locator("#package-name").fill("AUTO_TEST_V4")
-    page.get_by_role("textbox", name="Enter package description...").dblclick()
-    page.get_by_role("textbox", name="Enter package description...").fill("")
     page.get_by_text("Description is required").click()
-    page.get_by_role("button", name="Save").click()
